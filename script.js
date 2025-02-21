b96b20ca03949eeb50c759892edfa7b46ec9a421
ella-tang <tangjiayun45@gmail.com>
2025-02-20 23:05:56 -0500

    fix

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const isProjectsPage = window.location.pathname.includes("projects");
 const isPlaygroundPage = window.location.pathname.includes("playground");
 const isAboutPage = window.location.pathname.includes("about");
 
+if (window.location.pathname.endsWith("index.html")) {
+  window.location.replace(window.location.pathname.replace("index.html", ""));
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   setupCustomCursor();
   typeHeaderText();
